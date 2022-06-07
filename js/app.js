95a64b3d5c1ef06f18353a9074b2f984c58d9b61
Brian4755 <brian.mccune1@yahoo.com>
2022-06-07 02:34:10 -0500

    add font animation for intro

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 const intro = document.querySelector(".intro")
 
 window.addEventListener('scroll', function() {
+  const scrollHeight = document.documentElement.scrollHeight - window.innerHeight
+
+  console.log(scrollHeight, 'height')
+
   const scrolled = window.scrollY
   console.log(scrolled)
 
-  if (scrolled >= '1000') {
-    intro.style.color = 'blue'
+  if (scrolled >= 500 && scrolled <= 900) {
+    let scroll = 0.7*(scrolled-500)
+    intro.style.fontSize = `${scroll}px`
   }
 })
