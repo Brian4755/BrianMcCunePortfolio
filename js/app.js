d76f61f05c1bbcd1ae6548480e6ed7cc7c9f5629
Brian4755 <brian.mccune1@yahoo.com>
2022-06-07 14:12:05 -0500

    animation for bio made

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const intro = document.querySelector(".intro")
 const button = document.querySelector("button")
+const introBio1 = document.querySelector(".introBio1")
+const introBio2 = document.querySelector(".introBio2")
 
 window.addEventListener('scroll', function() {
   const scrollHeight = document.documentElement.scrollHeight - window.innerHeight
@@ -13,6 +15,10 @@ window.addEventListener('scroll', function() {
     let scroll = 0.6*(scrolled-800)
     intro.style.fontSize = `${scroll}px`
   }
+
+  const scroll1 = window.scrollY - 2300
+  introBio1.style.left = `${scroll1}px`
+  introBio2.style.right = `${scroll1}px`
 })
 
 button.addEventListener('click', function() {
