72b0e9c1e6089698b53a315d958f26c56b115477
Brian4755 <brian.mccune1@yahoo.com>
2022-06-07 00:59:51 -0500

    add a few scroll functions to intro

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-console.log('sanity check')
+const intro = document.querySelector(".intro")
+
+window.addEventListener('scroll', function() {
+  const scrolled = window.scrollY
+  console.log(scrolled)
+
+  if (scrolled >= '1000') {
+    intro.style.color = 'blue'
+  }
+})
