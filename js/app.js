134103ed63d0d4721b23d1d7b04850764644af72
Brian4755 <brian.mccune1@yahoo.com>
2022-06-08 08:29:34 -0500

    fix layout issues

--- js/app.js
+++ js/app.js
@@ -11,12 +11,12 @@ window.addEventListener('scroll', function() {
   const scrolled = window.scrollY
   console.log(scrolled)
 
-  if (scrolled >= 800 && scrolled <= 1100) {
-    let scroll = 0.6*(scrolled-800)
+  if (scrolled >= 1000 && scrolled <= 1300) {
+    let scroll = 0.8*(scrolled-1000)
     intro.style.fontSize = `${scroll}px`
   }
 
-  const scroll1 = window.scrollY - 2800
+  const scroll1 = window.scrollY - 2660
   introBio1.style.left = `${scroll1}px`
   introBio2.style.right = `${scroll1}px`
 })
@@ -24,3 +24,7 @@ window.addEventListener('scroll', function() {
 button.addEventListener('click', function() {
   window.scrollTo(0,0)
 })
+
+// const mediaQuery = window.matchMedia('(max-width: 900px)')
+
+// mediaQuery.addEventListener()
