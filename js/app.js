50a7fca7ce3d9fb7a5c219fcdfede4b05d0d2615
Brian4755 <brian.mccune1@yahoo.com>
2022-06-09 13:24:40 -0500

    mobile changes

--- js/app.js
+++ js/app.js
@@ -19,6 +19,21 @@ window.addEventListener('scroll', function() {
   const scroll1 = window.scrollY - 2700
   introBio1.style.left = `${scroll1}px`
   introBio2.style.right = `${scroll1}px`
+
+  
+  const smallWindowSize = window.matchMedia("(max-width: 600px")
+
+    if (smallWindowSize.matches) {
+      if (scrolled >= 1000 && scrolled <= 1300) {
+        let scroll = 0.4*(scrolled-1000)
+        intro.style.fontSize = `${scroll}px`
+      }
+
+      // const scroll1 = window.scrollY - 2700
+      // introBio1.style.left = `${scroll1}px`
+      // introBio2.style.right = `${scroll1}px`
+    }
+
 })
 
 button.addEventListener('click', function() {
