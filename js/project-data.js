da88425eded6e883d02d3faf832cbb769f7cf5e4
Brian4755 <brian.mccune1@yahoo.com>
2022-06-14 17:48:27 -0500

    add interactive work section

--- js/project-data.js
+++ js/project-data.js
@@ -1,15 +1,18 @@
+import { projectData } from "./works.js"
+
 const intro = document.querySelector(".intro")
 const button = document.querySelector("button")
 const introBio1 = document.querySelector(".introBio1")
 const introBio2 = document.querySelector(".introBio2")
+const cardContainer = document.getElementById('card-container')
 
 window.addEventListener('scroll', function() {
   const scrollHeight = document.documentElement.scrollHeight - window.innerHeight
 
-  console.log(scrollHeight, 'height')
+  // console.log(scrollHeight, 'height')
 
   const scrolled = window.scrollY
-  console.log(scrolled)
+  // console.log(scrolled)
 
   if (scrolled >= 1000 && scrolled <= 1300) {
     let scroll = 0.8*(scrolled-1000)
@@ -35,3 +38,24 @@ window.addEventListener('scroll', function() {
 button.addEventListener('click', function() {
   window.scrollTo(0,0)
 })
+
+const projectMaterial = projectData.map(project => {
+  const html = `  
+    <div class="card" style="width: 18rem;">
+      <img class="card-img-top" src="${project.image}" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${project.title}</h5>
+        <p class="card-text">${project.description}</p>
+        <div>
+          <a href="${project.gitHub}" target="_blank" class="btn btn-primary">GitHub</a>
+          <a href="${project.deployment}" target="_blank" class="btn btn-secondary">Deployment</a>
+        </div>
+      </div>
+    </div>
+  `
+  return html
+}).join('')
+
+
+cardContainer.innerHTML = projectMaterial
+console.log(projectMaterial)
---
+++ js/works.js
@@ -0,0 +1,13 @@
+let projectData = [
+  {
+    title: 'Tic Tac Toe',
+    deployment: 'https://tictactoe-brian4755.netlify.app/',
+    image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/284209771_971996506825292_3533046324500045364_n.png?stp=dst-png_s403x403&_nc_cat=111&ccb=1-7&_nc_sid=aee45a&_nc_ohc=rBvc1kQAuqwAX-3oGFN&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVL6rRtJN_XXaWT-g4BdcGY0Jr8ir-0aE2rt-mKzqqFwMQ&oe=62C3FC10',
+    gitHub: 'https://github.com/Brian4755/ttt-weekend',
+    description: 'Tic Tac Toe game using JS, HTML, and CSS'
+  }
+]
+
+export {
+  projectData
+}
