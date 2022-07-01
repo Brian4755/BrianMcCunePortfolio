1846ca9eda601ff41073317a9c4da0980fe73cba
Brian4755 <brian.mccune1@yahoo.com>
2022-07-01 08:39:31 -0500

    TFT cheatsheet deployment add

--- js/works.js
+++ js/works.js
@@ -5,6 +5,13 @@ let projectData = [
     image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/284209771_971996506825292_3533046324500045364_n.png?stp=dst-png_s403x403&_nc_cat=111&ccb=1-7&_nc_sid=aee45a&_nc_ohc=rBvc1kQAuqwAX-3oGFN&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVL6rRtJN_XXaWT-g4BdcGY0Jr8ir-0aE2rt-mKzqqFwMQ&oe=62C3FC10',
     gitHub: 'https://github.com/Brian4755/ttt-weekend',
     description: 'Tic Tac Toe game using JS, HTML, and CSS'
+  },
+  {
+    title: 'TFT CheatSheet',
+    deployment: 'https://tftcheatsheet.herokuapp.com/',
+    image: 'https://scontent.xx.fbcdn.net/v/t1.15752-9/290905112_996035144413563_3849728889679033423_n.png?stp=dst-png_p403x403&_nc_cat=110&ccb=1-7&_nc_sid=aee45a&_nc_ohc=fGPfM-mb_uQAX8FdBcH&_nc_oc=AQk9BzXSSXhFuHtdnFYG-e_cnHXHW3gi69aJsL_3_Rbkk8ec51lcu0YYxxfT_6tEZqCi0x6f8NX4OJcKYxPVjzyo&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVIzBPP-LCjMnZVtCRFrJUdoHTntxbsNEdt5jVaj64q3fQ&oe=62E612E2',
+    gitHub: 'https://github.com/Brian4755/TFTCheatSheet',
+    description: 'Forum/guide for a game called TeamFight Tactics. Used technologies such as EJS mongoDB/mongoose and JavaScript'
   }
 ]
 
